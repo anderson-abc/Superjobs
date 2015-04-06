d487d755c0fe800ecbbceee6972fc2dc03e01eb8
Hafedh AMRI <amrihafedh@yahoo.fr>
2015-04-06 18:23:04 +0200

    Add category options

--- src/Superjobs/HomeBundle/Form/JobsType.php
+++ src/Superjobs/HomeBundle/Form/JobsType.php
@@ -5,7 +5,6 @@ namespace Superjobs\HomeBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Superjobs\HomeBundle\Entity\JobsRepository; 
 
 class JobsType extends AbstractType
 {
