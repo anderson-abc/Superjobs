bd33e9d8243e1fa8a6084600a0040a27846270dd
Hafedh AMRI <amrihafedh@yahoo.fr>
2015-04-18 23:06:20 +0200

    Update things

--- src/Superjobs/HomeBundle/Controller/MainController.php
+++ src/Superjobs/HomeBundle/Controller/MainController.php
@@ -3,19 +3,27 @@
 namespace Superjobs\HomeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getEntityManager();
         $jobs = $em->getRepository("SuperjobsHomeBundle:Jobs")->findBy(   
                                 array(), 
                                 array('id' => 'DESC')
                 );
+        
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $jobs,
+            $request->query->get('page', 1)/*page number*/,
+            5/*limit per page*/
+        );
              
         return $this->render('SuperjobsHomeBundle:Main:index.html.twig', array(
-            'jobs'=>$jobs
+            'pagination' => $pagination
                 ));
     }
 }
