e147060d45492de9575d2f84fbb0fa720da91501
Hafedh AMRI <amrihafedh@yahoo.fr>
2015-06-09 23:42:05 +0200

    PUGX_Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Genemu\Bundle\FormBundle\GenemuFormBundle(),
+            new PUGX\MultiUserBundle\PUGXMultiUserBundle(),
         );
         
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
