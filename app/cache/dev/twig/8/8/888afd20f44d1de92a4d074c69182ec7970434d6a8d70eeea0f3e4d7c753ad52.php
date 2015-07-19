72fd4b115e5869061b06621579a2539caa9a13e5
Hafedh AMRI <amrihafedh@yahoo.fr>
2015-07-19 14:18:59 +0200

    Adding inro page for applicant interface

--- app/cache/dev/twig/8/8/888afd20f44d1de92a4d074c69182ec7970434d6a8d70eeea0f3e4d7c753ad52.php
+++ app/cache/dev/twig/8/8/888afd20f44d1de92a4d074c69182ec7970434d6a8d70eeea0f3e4d7c753ad52.php
@@ -36,13 +36,18 @@ class __TwigTemplate_888afd20f44d1de92a4d074c69182ec7970434d6a8d70eeea0f3e4d7c75
         <!-- Heading Row -->
         <div class=\"row\">
             <div class=\"col-md-8\">
-                <img class=\"img-responsive img-rounded\" src=\"http://placehold.it/900x350\" alt=\"\">
-            </div>
+                <img class=\"img-responsive img-rounded\" src=\"";
+        // line 11
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/superjobshome/img/candidat-avantages.jpg"), "html", null, true);
+        echo "\" width=\"100%\">
+";
+        // line 13
+        echo "            </div>
             <!-- /.col-md-8 -->
             <div class=\"col-md-4\">
                 <h1>Meilleur façons de postuler</h1>
                 <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
-                <a class=\"btn btn-primary btn-lg\" href=\"#\">Call to Action!</a>
+                <a class=\"btn btn-primary btn-lg\" href=\"#\">INSCRIPTION</a>
             </div>
             <!-- /.col-md-4 -->
         </div>
@@ -112,6 +117,6 @@ class __TwigTemplate_888afd20f44d1de92a4d074c69182ec7970434d6a8d70eeea0f3e4d7c75
 
     public function getDebugInfo()
     {
-        return array (  31 => 3,  28 => 2,  11 => 1,);
+        return array (  45 => 13,  41 => 11,  31 => 3,  28 => 2,  11 => 1,);
     }
 }
