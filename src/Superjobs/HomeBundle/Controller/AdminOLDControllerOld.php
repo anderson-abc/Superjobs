829f2710205e0cdcf9d02cd842969c2d518b9564
Hafedh AMRI <amrihafedh@yahoo.fr>
2016-10-29 22:05:51 +0200

    Upgrade - cleanup

---
+++ src/Superjobs/HomeBundle/Controller/AdminOLDControllerOld.php
@@ -0,0 +1,12 @@
+<?php
+// use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// use Symfony\Component\HttpFoundation\Response;
+//
+//class AdminController extends Controller
+//{
+//	public function adminAction()
+//	{
+//        return $this->render('SuperjobsHomeBundle:Main:admin.html.twig');
+//	}
+//}
